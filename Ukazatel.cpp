5d53b9124bf7a15621d3e93f2dae6da61f3f553f
VladimirMironov <44780782+vladimirVFX@users.noreply.github.com>
2020-04-28 17:52:02 +0300

    Add files via upload

---
+++ Ukazatel.cpp
@@ -0,0 +1,28 @@
+﻿// Ukazatel.cpp 
+
+#include <iostream>
+#include <cstdlib> 
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251>nul");
+    
+    char symb;
+    int num;
+    
+    char* p;
+    int* q;
+    
+    p = &symb;
+    q = &num;
+
+    *p = 'A';
+    *q = 100;
+
+    cout << "symb = " << symb << endl;
+    cout << "num = " << num << endl;
+}
+
+
